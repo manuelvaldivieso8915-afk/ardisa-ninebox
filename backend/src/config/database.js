30a659c663995b7a9b1923f0e5ea52f3cab63d05
manuelvaldivieso8915-afk <manuel.valdivieso8915@gmail.com>
2026-05-21 14:51:00 -0500

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false, requestCert: false },
+  ssl: false,
   max: 5,
 });
 
